34265c8d97a19124960d23e5d5f7b1b96dd371c0
Aurino Geraldo <aurinoaj@gmail.com>
2021-08-10 10:46:54 -0300

    Create header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,28 @@
+import "./header.styles.scss";
+
 export function Header() {
-  return <h1>Teste</h1>;
+  return (
+    <>
+      <header className="header">
+        <div className="header__container">
+          <h2 className="header__title">Aurino Geraldo</h2>
+
+          <nav className="header__menu">
+            <a className="header__menu__item" href="/">
+              Sobre
+            </a>
+            <a className="header__menu__item" href="/">
+              Projeto
+            </a>
+            <a className="header__menu__item" href="/">
+              Carreira
+            </a>
+            <a className="header__menu__item" href="/">
+              Contato
+            </a>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
 }
