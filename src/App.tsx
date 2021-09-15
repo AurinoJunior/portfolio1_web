7d942f164dd0fd5d3b181adbcb3cb04283b8b2ae
Aurino Geraldo <aurinoaj@gmail.com>
2021-09-15 20:56:58 -0300

    Add responsive style on Carrer section

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 import { About } from "./components/About";
 // import { Projects } from "./components/Projects";
-// import { Career } from "./components/Career";
+import { Career } from "./components/Career";
 // import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 
@@ -17,7 +17,7 @@ function App() {
       <Home />
       <About />
       {/* <Projects /> */}
-      {/* <Career /> */}
+      <Career />
       {/* <Contact /> */}
       <Footer />
     </>
