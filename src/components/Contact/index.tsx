1c5230125fb161a62f3342129d7a81ae8bf3ef70
Aurino Geraldo <aurinoaj@gmail.com>
2021-09-15 17:27:47 -0300

    Add animation (#3)
    * Reafctor code and add animation lib
    
    * Add email in contact
    
    * * Create function for copy discord id
    * Add images on contact section
    * Change bottom space titles

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,27 +1,49 @@
 import "./contact.styles.scss";
 
+import instagramImage from '../../assets/images/instagram.png'
+import discordImage from '../../assets/images/discord.png'
+import linkedinImage from '../../assets/images/linkedin.png'
+import githubImage from '../../assets/images/github.png'
+import { useState } from "react";
+
 export function Contact() {
+  const [discordText, setDiscordText] = useState('Aurigod#4050')
+
+
+  function handleCopyDiscordId(){
+    navigator.clipboard.writeText(discordText)
+    setDiscordText('Copiado')
+
+    setTimeout(() => {
+      setDiscordText('Aurigod#4050')
+    }, 2000)
+  }
+
+
   return (
     <section id="contact" className="contact">
       <h2 className="contact__title">Contatos</h2>
       <div className="contact__content">
-        <div className="contact__item">
-          <img src="https://via.placeholder.com/80" alt="Instagram" />
+        <a className="contact__item" href="https://www.instagram.com/aurigod97/">
+          <img src={instagramImage} alt="Instagram" width="64"/>
           <span>Instagram</span>
+        </a>
+
+        <div className="contact__item" onClick={handleCopyDiscordId}>
+          <img src={discordImage} alt="Discord" width="64"/>
+          <span className={discordText === 'Copiado' ? 'item-copy' : ''}>{discordText}</span>
         </div>
-        <div className="contact__item">
-          <img src="https://via.placeholder.com/80" alt="Discord" />
-          <span>Discord</span>
-        </div>
-        <div className="contact__item">
-          <img src="https://via.placeholder.com/80" alt="Linkedin" />
+
+        <a className="contact__item" href="https://www.linkedin.com/in/aurino-junior-7718a4158/">
+          <img src={linkedinImage} alt="Linkedin" width="64"/>
           <span>Linkedin</span>
-        </div>
-        <div className="contact__item">
-          <img src="https://via.placeholder.com/80" alt="Github" />
+        </a>
+        <a className="contact__item" href="https://github.com/AurinoJunior">
+          <img src={githubImage} alt="Github" width="64"/>
           <span>Github</span>
-        </div>
+        </a>
       </div>
+      <h3 className="contact__email">Email: aurinoaj@gmail.com</h3>
     </section>
   );
 }
