ab1096c06442312ebf9d25f0987ddfd3bd4e404b
Aurino Geraldo <aurinoaj@gmail.com>
2021-09-15 19:39:47 -0300

    Create burger menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,30 +1,50 @@
+import { useState } from "react";
+import "animate.css";
+
 import "./header.styles.scss";
 
+import closeIcon from "../../assets/images/close.svg";
+import menuIcon from "../../assets/images/menu.svg";
+
 export function Header() {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  function handleChangeStateMenu() {
+    setMenuIsOpen(!menuIsOpen);
+  }
+
   return (
     <>
-      <header className="header">
-        <div className="header__content">
-          <h1 className="header__title">
-            <a href="#home">Aurino Geraldo</a>
-          </h1>
+      <img
+        className={menuIsOpen ? "header__close" : "header__open"}
+        src={menuIsOpen ? closeIcon : menuIcon}
+        alt="Fechar"
+        onClick={handleChangeStateMenu}
+      />
 
-          <nav className="header__menu">
-            <a className="header__menu__item" href="#about">
-              Sobre
-            </a>
-            <a className="header__menu__item" href="#projects">
-              Projeto
-            </a>
-            <a className="header__menu__item" href="#career">
-              Carreira
-            </a>
-            <a className="header__menu__item" href="#contact">
-              Contatos
-            </a>
-          </nav>
-        </div>
-      </header>
+      {menuIsOpen && (
+        <header className="header">
+          <div className="header__content animate__animated animate__slideInRight">
+            <nav className="header__menu">
+              <a className="header__menu__item" href="#home">
+                Inicio
+              </a>
+              <a className="header__menu__item" href="#about">
+                Sobre
+              </a>
+              <a className="header__menu__item" href="#projects">
+                Projeto
+              </a>
+              <a className="header__menu__item" href="#career">
+                Carreira
+              </a>
+              <a className="header__menu__item" href="#contact">
+                Contatos
+              </a>
+            </nav>
+          </div>
+        </header>
+      )}
     </>
   );
 }
